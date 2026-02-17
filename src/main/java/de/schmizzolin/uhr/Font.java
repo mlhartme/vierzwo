739a12ead19325fef67b9d5a4d33fb2f80f86b8c
Michael Hartmeier <mlhartme.users.noreply@github.com>
2026-02-17 21:39:59 +0100

    cleanup

--- src/main/java/de/schmizzolin/uhr/Font.java
+++ src/main/java/de/schmizzolin/uhr/Font.java
@@ -83,14 +83,14 @@ public class Font {
 
     public static Font create(Color color, int dotSize) {
         Font font = new Font(color, dotSize, dotSize / 2);
-        font.add(':', Matrix.create(COLON));
+        font.add(':', COLON);
         for (int i = 0; i < DIGITS.length; i++) {
-            font.add((char) ('0' + i), Matrix.create(DIGITS[i]));
+            font.add((char) ('0' + i), DIGITS[i]);
         }
         return font;
     }
 
-    private final Map<Character, Matrix> maps; // value[y][x]
+    private final Map<Character, Matrix> dots; // value[y][x]
     private final Color color;
     private final int dotWidth;
     private final int dotSpace;
@@ -99,18 +99,20 @@ public class Font {
 
     public Font(Color color, int dotWidth, int charSpace) {
         this.color = color;
-        this.maps = new HashMap<>();
+        this.dots = new HashMap<>();
         this.dotWidth = dotWidth;
         this.dotSpace = Math.max(1, dotWidth / 32);
         this.dotArc = Math.max(1, dotWidth / 6);
         this.charSpace = charSpace;
     }
 
-    public void add(char character, Matrix matrix) {
-        maps.put(character, matrix);
+    public void add(char character, String matrix) {
+        dots.put(character, Matrix.create(matrix));
     }
 
-    private static final int HEIGHT = 5;
+    public void add(char character, Matrix matrix) {
+        dots.put(character, matrix);
+    }
 
     public Group render(String str) {
         Group result = new Group();
@@ -121,7 +123,7 @@ public class Font {
     }
 
     private void render(Group dest, int ofs, char character) {
-        Matrix matrix = maps.get(character);
+        Matrix matrix = dots.get(character);
         if (matrix == null) {
             throw new IllegalArgumentException("unknown character: " + character);
         }
